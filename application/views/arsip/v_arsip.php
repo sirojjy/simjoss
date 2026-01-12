e05fefd3dc2ca5a257d125e4873e10ee226e6da8
Taufik Ridho <taufikridho505@gmail.com>
2026-01-12 11:53:52 +0700

    fix: typo

--- application/views/arsip/v_arsip.php
+++ application/views/arsip/v_arsip.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand font-weight-bold" href="javascript:void(0);">Pencaraian Arsip </a>
+    <a class="navbar-brand font-weight-bold" href="javascript:void(0);">Pencarian Arsip </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fa fa-align-justify"></i>
     </button>
